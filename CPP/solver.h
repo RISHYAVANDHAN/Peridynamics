889ee408158c45ea358e00ac528c562e3886c4c6
Anips061 <aniruddha.p.shastry@fau.de>
2025-03-11 12:26:14 +0100

    updated certain small bugs

--- CPP/solver.h
+++ CPP/solver.h
@@ -28,7 +28,7 @@ inline Eigen::Matrix3d compute_Kab_1(const Eigen::Vector3d& xi_1, double L, doub
 
     Eigen::Matrix3d term1 = (1.0 / l_cubed) * xi_outer;
     Eigen::Matrix3d identity = Eigen::Matrix3d::Identity();
-    double scalar_term = ((1.0 / L) - (1.0 / l));
+    const double scalar_term = ((1.0 / L) - (1.0 / l));
 
     Kab = C1 * V_eff * (term1 + scalar_term * identity);
     return Kab;
@@ -36,26 +36,26 @@ inline Eigen::Matrix3d compute_Kab_1(const Eigen::Vector3d& xi_1, double L, doub
 
 inline Eigen::Matrix3d compute_Kab_2(const Eigen::Vector3d& xi_1, const Eigen::Vector3d& xi_2, const Eigen::Vector3d& Xi_1, const Eigen::Vector3d& Xi_2, double A, double a, double V_eff, double C1) {
     Eigen::Matrix3d Kab = Eigen::Matrix3d::Zero();
-    Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
+    const Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
 
-    double xi1_dot_xi2 = xi_1.dot(xi_2);
-    double xi2_dot_xi2 = xi_2.dot(xi_2);
-    double xi1_dot_xi1 = xi_1.dot(xi_1);
+    const double xi1_dot_xi2 = xi_1.dot(xi_2);
+    const double xi2_dot_xi2 = xi_2.dot(xi_2);
+    const double xi1_dot_xi1 = xi_1.dot(xi_1);
 
     Eigen::Vector3d term_vec1 = xi1_dot_xi2 * xi_2 - xi2_dot_xi2 * xi_1;
     Eigen::Vector3d term_vec2 = xi1_dot_xi2 * xi_1 - xi1_dot_xi1 * xi_2;
 
-    Eigen::Matrix3d term1 = (2.0 * C1 * V_eff / std::pow(a, 3)) * (term_vec1 * term_vec1.transpose());
-    double scalar_term = (1.0 / a) - (1.0 / A);
-    Eigen::Matrix3d term2 = (2.0 * C1 * V_eff * scalar_term) * (xi_2 * xi_2.transpose() - xi1_dot_xi2 * I);
-    Eigen::Matrix3d term3 = (2.0 * C1 * V_eff / std::pow(a, 3)) * (term_vec1 * term_vec2.transpose());
-    Eigen::Matrix3d term4 = (2.0 * C1 * V_eff * scalar_term) * (xi_2 * xi_1.transpose() + xi1_dot_xi2 * I - 2.0 * xi_1 * xi_2.transpose());
+    const Eigen::Matrix3d term1 = (2.0 * C1 * V_eff / std::pow(a, 3)) * (term_vec1 * term_vec1.transpose());
+    const double scalar_term = (1.0 / a) - (1.0 / A);
+    const Eigen::Matrix3d term2 = (2.0 * C1 * V_eff * scalar_term) * (xi_2 * xi_2.transpose() - xi1_dot_xi2 * I);
+    const Eigen::Matrix3d term3 = (2.0 * C1 * V_eff / std::pow(a, 3)) * (term_vec1 * term_vec2.transpose());
+    const Eigen::Matrix3d term4 = (2.0 * C1 * V_eff * scalar_term) * (xi_2 * xi_1.transpose() + xi1_dot_xi2 * I - 2.0 * xi_1 * xi_2.transpose());
 
     Kab = term1 + term2 + term3 + term4;
     return Kab;
 }
 
-inline Eigen::Matrix3d compute_Kab_3(const Eigen::Vector3d& xi_1, const Eigen::Vector3d& xi_2, const Eigen::Vector3d& xi_3, const Eigen::Vector3d& Xi_1, const Eigen::Vector3d& Xi_2, const Eigen::Vector3d& Xi_3, double V, double v, double V_eff, double C1) {
+inline Eigen::Matrix3d compute_Kab_3(const Eigen::Vector3d& xi_1, const Eigen::Vector3d& xi_2, const Eigen::Vector3d& xi_3, const Eigen::Vector3d& Xi_1, const Eigen::Vector3d& Xi_2, const Eigen::Vector3d& Xi_3, double V, double v, const double V_eff, const double C1) {
     Eigen::Matrix3d Kab = Eigen::Matrix3d::Zero();
 
     Eigen::Vector3d xi_12 = xi_1.cross(xi_2);
@@ -87,11 +87,15 @@ inline void calculate_r(int PD, std::vector<Points>& point_list, double NN, doub
     const double pi = 3.14159265358979323846;
     double Vh = (4.0 / 3.0) * pi * (delta * delta * delta);
 
+
     for (size_t i = 0; i < point_list.size(); i++) {
         point_list[i].psi = 0.0;
         point_list[i].Ra_1 = Eigen::Vector3d::Zero();
         point_list[i].Ra_2 = Eigen::Vector3d::Zero();
         point_list[i].Ra_3 = Eigen::Vector3d::Zero();
+        point_list[i].Kab_1.setZero();
+        point_list[i].Kab_2.setZero();
+        point_list[i].Kab_3.setZero();
 
         for (size_t j = 0; j < point_list[i].neighbour_list_1N.size(); j++) {
             int neighbor_idx = point_list[i].neighbour_list_1N[j][0];
@@ -103,10 +107,11 @@ inline void calculate_r(int PD, std::vector<Points>& point_list, double NN, doub
             double l = xi_1.norm();
 
             point_list[i].psi += compute_psi(C1, L, l);
-            point_list[i].V_eff = Vh / point_list[i].n1;
+            point_list[i].V_eff = Vh / static_cast<double>(point_list[i].n1);
 
             point_list[i].Ra_1 += xi_1 * C1 * point_list[i].V_eff * ((1 / L) - (1 / l));
             point_list[i].Kab_1 += compute_Kab_1(xi_1, L, l, point_list[i].V_eff, C1);
+
         }
 
         for (size_t j = 0; j < point_list[i].neighbour_list_2N.size(); j++) {
@@ -125,7 +130,7 @@ inline void calculate_r(int PD, std::vector<Points>& point_list, double NN, doub
             double a = xi.norm();
 
             point_list[i].psi += compute_psi(C1, A, a);
-            point_list[i].V_eff = (Vh * Vh) / (point_list[i].n1 + point_list[i].n2);
+            point_list[i].V_eff = (Vh * Vh) / static_cast<double>(point_list[i].n1 + point_list[i].n2);
 
             Eigen::Vector3d calc = (xi_2.dot(xi_2) * xi_1) - (xi_1.dot(xi_2) * xi_2);
             point_list[i].Ra_2 += 2 * C1 * ((1 / A) - (1 / a)) * point_list[i].V_eff * calc;
@@ -151,7 +156,7 @@ inline void calculate_r(int PD, std::vector<Points>& point_list, double NN, doub
             double v = xi;
 
             point_list[i].psi += compute_psi(C1, V, v);
-            point_list[i].V_eff = (Vh * Vh * Vh) / (point_list[i].n1 + point_list[i].n2 + point_list[i].n3);
+            point_list[i].V_eff = (Vh * Vh * Vh) / static_cast<double>(point_list[i].n1 + point_list[i].n2 + point_list[i].n3);
 
             double calc = xi_2.dot(xi_1.cross(xi_2));
             Eigen::Vector3d calc1 = xi_2.cross(xi_3);
--- CPP/update.h
+++ CPP/update.h
@@ -11,23 +11,32 @@
 #include "Points.h"
 #include "Neighbour.h"
 
-std::vector<Points> update_prescribed(std::vector<Points>& point_list, const int LF, const int PD, const double delta) {
+inline std::vector<Points> update_prescribed(std::vector<Points>& point_list, double LF, const int PD, const double delta) {
     for (auto& i : point_list) {
-        for (int j = 0; j < PD; j++) {
-            i.x[j] = i.X[j] + LF * i.BCval[j];
+        for (int j = 0; j < PD; j++){
+            if (i.BC[j] == 0)
+            {
+                i.x[j] = i.X[j] + LF * i.BCval[j];
+            }
         }
     }
     generate_neighbour_list(PD, point_list, delta);
     return point_list;
 }
 
-std::vector<Points> update_displaced(std::vector<Points>& point_list, const Eigen::VectorXd& dx, const int PD, const double delta) {
+inline std::vector<Points> update_displaced(std::vector<Points>& point_list, const Eigen::VectorXd& dx, const int PD, const double delta) {
     for (auto& i : point_list) {
         for (int j = 0; j < PD; j++) {
+            if (i.BC[j] == 1) {
             // Make sure the DOF index is within valid range
-            int dof_idx = static_cast<int>(i.DOF(j));
-            if (dof_idx >= 0 && dof_idx < dx.size()) {
+            int dof_idx = static_cast<int>(i.DOF[j]);
+            if (dof_idx >= 0 && dof_idx < dx.size())
+            {
                 i.x[j] = i.X[j] + dx.coeff(dof_idx);
+                std::cout << i.x[j] << std::endl;
+                std::cout << i.X[j] << std::endl;
+                std::cout << dx.coeff(dof_idx) << std::endl;
+            }
             }
         }
     }
